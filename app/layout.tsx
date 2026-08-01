9875bdc6c7a319adb8741b03a867d7184945b8fa
tharnyelilvannan <tharny2005@outlook.com>
2026-08-01 17:45:57 -0400

    remove duplicates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Archivo_Narrow, Alice, Julius_Sans_One, Space_Grotesk, Hanken_Grotesk, Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const alice = Alice({
-  variable: "--font-alice",
-  weight: "400",
-  subsets: ["latin"],
-});
-
-const juliusSansOne = Julius_Sans_One({
-  variable: "--font-julius-sans-one",
-  weight: "400",
-  subsets: ["latin"],
-});
-
 const archivoNarrow = Archivo_Narrow({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
